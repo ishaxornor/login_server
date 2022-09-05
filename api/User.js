5179058aea33c532930428f25c2f5b9b28d5e35f
Isha Guravaleh <isha.xornor@gmail.com>
2022-09-05 17:35:18 +0530

    first commit

--- api/User.js
+++ api/User.js
@@ -96,7 +96,61 @@ router.post('/signup', (req, res) => {
 
 //signin
 router.post('/signin', (req, res) => {
+    let { email , password} = req.body;
+    email = email.trim();
+    password = password.trim();
 
+    if(email == "" || password == ""){
+        res.json({
+            status:"FAILED",
+            message: "Empty credentials supplied"
+        })
+    } else{
+        //check if user exists
+        User.find({email})
+        .then(data => {
+            if(data) {
+                //user exists
+
+                const hashedPassword = data[0].password;
+                bcrypt.compare(password,hashPassword).then(result => {
+                    if(result) {
+                        //password match
+                        res.json({
+                            status:"SUCCESS",
+                            message: "Signin successful",
+                            data: data
+                        })
+                    }else{
+                         res.json({
+                            status:"FAILED",
+                            message: "Invalid password entered"
+                         })
+                    }
+                })
+                .catch(err => {
+                    res.json({
+                        status: "FAILED",
+                        message:"An error occurred while comparing passwords"
+                })
+
+            })
+            }else{
+                res.json({
+                    status : "FAILED",
+                    message: "Invalid credentials"
+                })
+            }
+        })
+        .catch(err => {
+            res.json({
+                status : "FAILED",
+                message: "An error occurred while checking for existing user"
+            })
+        })
+
+    }
+    
 })
 
 module.exports = router; 
--- server.js
+++ server.js
@@ -3,9 +3,13 @@
 require('./config/db');
 
 const app = require('express')();
-const port = 4000;
+const port = 8000;
+
+const cors = require("cors");
+app.use(cors());
 
 const UserRouter = require('./api/User');
+
 
 //for accessing post form data
 
